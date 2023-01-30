ed3bdd8a66e97973a9c5e45a818da8676fde82d7
raghavtilak <tilakddsharma@gmail.com>
2023-01-31 01:12:57 +0530

    1. Dynamic Drawable for background and foreground view 2. Add views to coumpound view from xml

--- app/src/main/java/com/raghav/neubrutalism/NeuBrutalism.kt
+++ app/src/main/java/com/raghav/neubrutalism/NeuBrutalism.kt
@@ -8,10 +8,10 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import android.widget.RelativeLayout
-import android.widget.TextView
 
-class NeuBrutalism : RelativeLayout {
+class NeuBrutalism(context: Context, attrs: AttributeSet?) : RelativeLayout(context, attrs) {
 
     var bg_marginTop = 6
     var bg_marginStart = 6
@@ -21,8 +21,11 @@ class NeuBrutalism : RelativeLayout {
     val fg = GradientDrawable()
     val bg = GradientDrawable()
 
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+    var layout : RelativeLayout
+    var bgView : View
 
+
+    init {
         val a = context.obtainStyledAttributes(
             attrs,
             R.styleable.NeuBrutalism, 0, 0
@@ -32,21 +35,15 @@ class NeuBrutalism : RelativeLayout {
         bg_radius = a.getDimensionPixelSize(R.styleable.NeuBrutalism_bg_radius, 3)
         fg_stroke = a.getDimensionPixelSize(R.styleable.NeuBrutalism_fg_stroke, 3)
         a.recycle()
-
     }
 
     init {
 
-        inflate(context,R.layout.neubrutalismbutton,this)
-        val textview = findViewById<TextView>(R.id.textview)
-        val bgView = findViewById<View>(R.id.bgView)
-        val layout = findViewById<RelativeLayout>(R.id.layout)
+        inflate(context, R.layout.neubrutalismbutton, this)
+        layout = findViewById(R.id.layout)
+        bgView = findViewById(R.id.bgView)
 
         layout.viewTreeObserver.addOnGlobalLayoutListener {
-
-            val params = LayoutParams(textview.width, textview.height)
-            params.setMargins(bg_marginStart, bg_marginTop, 0, 0)
-            bgView.layoutParams = params
 
             bg.apply {
                 shape = GradientDrawable.RECTANGLE
@@ -54,19 +51,31 @@ class NeuBrutalism : RelativeLayout {
                 color = ColorStateList.valueOf(Color.BLACK)
             }
             bgView.background = bg
-
             fg.apply {
                 shape = GradientDrawable.RECTANGLE
                 cornerRadius = bg_radius.toFloat()
                 setStroke(fg_stroke, ColorStateList.valueOf(Color.BLACK))
                 color = ColorStateList.valueOf(Color.WHITE)
             }
-            textview.background = fg
         }
     }
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    )
+
+    override fun addView(child: View, index: Int, p: ViewGroup.LayoutParams) {
+
+        if (child.id != R.id.layout) {
+
+            child.viewTreeObserver.addOnGlobalLayoutListener(object :
+                ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    child.background = fg
+
+                    val params = LayoutParams(child.width, child.height)
+                    params.setMargins(bg_marginStart, bg_marginTop, 0, 0)
+                    findViewById<View>(R.id.bgView).layoutParams = params
+                    child.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                }
+            })
+        }
+        super.addView(child, index, p)
+    }
 }
